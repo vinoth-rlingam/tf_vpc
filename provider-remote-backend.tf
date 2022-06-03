c0291b7c8e03e828cf26ead6122a3bd93f92be72
vinoth-rlingam <vinoth.rlingam@gmail.com>
2022-06-03 13:01:54 +1000

    Update provider-remote-backend.tf

--- provider-remote-backend.tf
+++ provider-remote-backend.tf
@@ -1,13 +1,6 @@
 terraform {
-  backend "s3" {}
-}
-
-data "terraform_remote_state" "remotestate" {
-  backend = "s3"
-
-  config = {
-    bucket = "s3statestore-vpctf"
-    region = "us-east-1"
-    key    = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "s3statestore-vpctf"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
   }
-}
